5ea98ea3b6fbc73784a4f1444165e18e0dd25ddc
Prasanna Bhogale <pbhogale@gmail.com>
2019-08-06 21:13:18 +0200

    beginning the basics

---
+++ run-in-terminal.sh
@@ -0,0 +1,16 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew cask install visual-studio-code
+brew cask install rstudio
+brew cask install r
+brew cask install sage
+brew cask install mactex
+brew cask install docker
+brew cask install tableplus
+brew cask install tunnelblick
+brew cask install julia
+brew install python
+brew install python@2
+brew install postgresql
+pip3 install jupyterlab
+pip3 install pandas
+pip3 install psycopg2
